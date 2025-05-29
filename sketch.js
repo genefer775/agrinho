8327985d152e479f0a94f63a4866d98f64ca69fe
genefer775 <genefer.maciel@escola.pr.gov.br>
2025-05-29 11:19:09 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+  createCanvas(700, 700)
+  square = createSprite(350, 350)
+  squareImg = loadImage('https://raw.githubusercontent.com/lissaferreira/criando-jogos-com-js-e-p5/main/src/assets/square.png')
+  square.addImage(squareImg)
+function setup(){
+  createCanvas(700, 700)
 }
 
-function draw() {
-  background(220);
+function draw(){
+  background(230)
 }
+
+ 
